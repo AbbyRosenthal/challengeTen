1b301c0a7494ad3af6a0b19c8dd3a4f6ae3fe77b
Abby Rosenthal <adlundeen@gmail.com>
2022-07-02 21:30:17 -0500

    intern test passing

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,1 +1,10 @@
 const Manager = require('../lib/Manager')
+
+test('creates a Manager object', () => {
+    const manager = new Manager("", "", "", "");
+
+    expect(manager.name).toEqual(expect.any(String));
+    expect(manager.id).toEqual(expect.any(String));
+    expect(manager.email).toEqual(expect.any(String));
+    expect(manager.officeNumber).toEqual(expect.any(String));
+});
